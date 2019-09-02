86ff83302d8a892ca9e1b5ac66e7c86d30cfae29
marmotcai <marmotcai@163.com>
2019-09-02 17:01:48 +0800

    init

--- predict/urls.py
+++ predict/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = "predict"
 
 urlpatterns = [
+    path("init/", views.init, name='init'),
     path("index/", views.index, name='index'),
     path("home/", views.home, name = 'home'),
     path("predict/", views.predict_stock_action, name = 'predict'),
